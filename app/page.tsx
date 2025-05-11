1114e4e5e857faa5cbfade32619189816998ebc2
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-05-11 17:36:36 +0800

    routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@/components/ui/button";
 
-
 export default function Home() {
   return (
     <div className="">
+      <h1 className="font-bold text-4xl text-primary">Hellow World</h1>
       <Button>Hallow World</Button>
     </div>
   );
