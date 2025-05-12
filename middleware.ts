37602dea65fdfb264af6da7851ab7a339cfa876a
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-05-12 11:09:34 +0800

    fix error middleware redirect

--- middleware.ts
+++ middleware.ts
@@ -21,7 +21,7 @@ export default auth((req)=>{
 
     if(isAuthRoute){
         if(isLoggedIn){
-            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT))
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
         }
         return 
     }
