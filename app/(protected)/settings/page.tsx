c31c24b34d981f291b6892489958dbd601f5c501
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-05-12 11:30:04 +0800

    sign-out functionality

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import { auth } from '@/auth'
+import { auth, signOut } from '@/auth'
 const SettingPage = async() => {
     const session = await auth()
     return (
-        <div>{JSON.stringify(session)}</div>
+        <div>
+            {JSON.stringify(session)}
+            <form action={async()=>{
+                "use server"
+                await signOut()
+            }}>
+                <button className='cursor-pointer p-4 bg-primary text-white hover:bg-primary/90 transition-all' type="submit">Sign Out</button>
+            </form>
+        </div>
     )
 }
 
