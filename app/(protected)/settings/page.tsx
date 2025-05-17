65a8cff6bc10a481ecf023de0ff9c2f0c7e75e4f
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-05-17 16:48:22 +0800

    new logout functionality

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,14 +1,22 @@
 "use client"
+import { logout } from '@/actions/logout'
 import { useSession } from 'next-auth/react'
 import React from 'react'
 const SettingPage = () => {
     const session =useSession()
+    const onClick=()=>{
+        logout()
+    }
     return (
         <div>
             {JSON.stringify(session)}
-            <form>
-                <button className='cursor-pointer p-4 bg-primary text-white hover:bg-primary/90 transition-all' type="submit">Sign Out</button>
-            </form>
+            <button 
+                onClick={onClick} 
+                className='cursor-pointer p-4 bg-primary text-white hover:bg-primary/90 transition-all' 
+                type="submit"
+            >
+                Sign Out
+            </button>
         </div>
     )
 }
